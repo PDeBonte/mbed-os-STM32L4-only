tung7970/mbed-os

Added a baud argument for (Raw)Serial objects
This commit adds a `baud` argument for Serial and RawSerial objects:

- there is a new `Serial` constructor with a mandatory baud rate
  argument. The old constructor also got a `baud` argument with a
  default value (to keep backward compatibility).
- the `RawSerial` constructor also got a `baud` argument with a default
  value.

There's also a new configuration parameter (`default-serial-baud-rate`)
that can be used to specify the default value of the above `baud`
arguments.

hal/api/RawSerial.h
@@ -50,15 +50,16 @@ namespace mbed {
 class RawSerial: public SerialBase {
 
 public:
-    /** Create a RawSerial port, connected to the specified transmit and receive pins
+    /** Create a RawSerial port, connected to the specified transmit and receive pins, with the specified baud.
      *
      *  @param tx Transmit pin
      *  @param rx Receive pin
+     *  @param baud The baud rate of the serial port (optional, defaults to MBED_CONF_CORE_DEFAULT_SERIAL_BAUD_RATE)
      *
      *  @note
      *    Either tx or rx may be specified as NC if unused
      */
-    RawSerial(PinName tx, PinName rx);
+    RawSerial(PinName tx, PinName rx, int baud = MBED_CONF_CORE_DEFAULT_SERIAL_BAUD_RATE);
 
     /** Write a char to the serial port
      *

hal/api/Serial.h
@@ -59,11 +59,25 @@ public:
      *
      *  @param tx Transmit pin
      *  @param rx Receive pin
+     *  @param name The name of the stream associated with this serial port (optional)
+     *  @param baud The baud rate of the serial port (optional, defaults to MBED_CONF_CORE_DEFAULT_SERIAL_BAUD_RATE)
      *
      *  @note
      *    Either tx or rx may be specified as NC if unused
      */
-    Serial(PinName tx, PinName rx, const char *name=NULL);
+    Serial(PinName tx, PinName rx, const char *name=NULL, int baud = MBED_CONF_CORE_DEFAULT_SERIAL_BAUD_RATE);
+
+
+    /** Create a Serial port, connected to the specified transmit and receive pins, with the specified baud
+     *
+     *  @param tx Transmit pin
+     *  @param rx Receive pin
+     *  @param baud The baud rate of the serial port
+     *
+     *  @note
+     *    Either tx or rx may be specified as NC if unused
+     */
+    Serial(PinName tx, PinName rx, int baud);
 
 protected:
     virtual int _getc();

PDeBonte/mbed-os-STM32L4-only

drivers/RawSerial.h
@@ -50,15 +50,16 @@ namespace mbed {
 class RawSerial: public SerialBase {
 
 public:
-    /** Create a RawSerial port, connected to the specified transmit and receive pins
+    /** Create a RawSerial port, connected to the specified transmit and receive pins, with the specified baud.
      *
      *  @param tx Transmit pin
      *  @param rx Receive pin
+     *  @param baud The baud rate of the serial port (optional, defaults to MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE)
      *
      *  @note
      *    Either tx or rx may be specified as NC if unused
      */
-    RawSerial(PinName tx, PinName rx);
+    RawSerial(PinName tx, PinName rx, int baud = MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
 
     /** Write a char to the serial port
      *

drivers/Serial.h
@@ -59,11 +59,25 @@ public:
      *
      *  @param tx Transmit pin
      *  @param rx Receive pin
+     *  @param name The name of the stream associated with this serial port (optional)
+     *  @param baud The baud rate of the serial port (optional, defaults to MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE)
      *
      *  @note
      *    Either tx or rx may be specified as NC if unused
      */
-    Serial(PinName tx, PinName rx, const char *name=NULL);
+    Serial(PinName tx, PinName rx, const char *name=NULL, int baud = MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE);
+
+
+    /** Create a Serial port, connected to the specified transmit and receive pins, with the specified baud
+     *
+     *  @param tx Transmit pin
+     *  @param rx Receive pin
+     *  @param baud The baud rate of the serial port
+     *
+     *  @note
+     *    Either tx or rx may be specified as NC if unused
+     */
+    Serial(PinName tx, PinName rx, int baud);
 
 protected:
     virtual int _getc();
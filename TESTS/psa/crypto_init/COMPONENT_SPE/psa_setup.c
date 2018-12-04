netanelgonen/mbed-os

Fix PSA crypto partiotion and tests
* Styling
* Correct error codes on failing connection
* Add panics where needed
* correct skip defines
* Fix psa_spm_init_refence_counter bug

TESTS/psa/crypto_init/COMPONENT_SPE/psa_setup.c
@@ -27,21 +27,21 @@
 #include "spm_internal.h"
 #include "handles_manager.h"
 #include "cmsis.h"
-#include "psa_psa_f_partition.h"
+#include "psa_crypto_partition.h"
 #include "psa_its_partition.h"
 
-extern const uint32_t psa_f_external_sids[4];
+extern const uint32_t crypto_external_sids[4];
 
 spm_partition_t g_partitions[2] = {
     {
-        .partition_id = PSA_F_ID,
+        .partition_id = CRYPTO_ID,
         .thread_id = 0,
-        .flags_rot_srv = PSA_F_WAIT_ANY_SID_MSK,
+        .flags_rot_srv = CRYPTO_WAIT_ANY_SID_MSK,
         .flags_interrupts = 0,
         .rot_services = NULL,
-        .rot_services_count = PSA_F_ROT_SRV_COUNT,
-        .extern_sids = psa_f_external_sids,
-        .extern_sids_count = PSA_F_EXT_ROT_SRV_COUNT,
+        .rot_services_count = CRYPTO_ROT_SRV_COUNT,
+        .extern_sids = crypto_external_sids,
+        .extern_sids_count = CRYPTO_EXT_ROT_SRV_COUNT,
         .irq_mapper = NULL,
     },
     {
@@ -65,7 +65,7 @@ const mem_region_t *mem_regions = NULL;
 const uint32_t mem_region_count = 0;
 
 // forward declaration of partition initializers
-void psa_f_init(spm_partition_t *partition);
+void crypto_init(spm_partition_t *partition);
 void its_init(spm_partition_t *partition);
 
 uint32_t init_partitions(spm_partition_t **partitions)
@@ -74,7 +74,7 @@ uint32_t init_partitions(spm_partition_t **partitions)
         SPM_PANIC("partitions is NULL!\n");
     }
 
-    psa_f_init(&(g_partitions[0]));
+    crypto_init(&(g_partitions[0]));
     its_init(&(g_partitions[1]));
 
     *partitions = g_partitions;

TESTS/psa/entropy_inject/COMPONENT_SPE/psa_setup.c
@@ -28,10 +28,10 @@
 #include "handles_manager.h"
 #include "cmsis.h"
 #include "psa_test_its_reset_partition.h"
-#include "psa_psa_f_partition.h"
+#include "psa_crypto_partition.h"
 #include "psa_its_partition.h"
 
-extern const uint32_t psa_f_external_sids[4];
+extern const uint32_t crypto_external_sids[4];
 
 spm_partition_t g_partitions[3] = {
     {
@@ -46,14 +46,14 @@ spm_partition_t g_partitions[3] = {
         .irq_mapper = NULL,
     },
     {
-        .partition_id = PSA_F_ID,
+        .partition_id = CRYPTO_ID,
         .thread_id = 0,
-        .flags_rot_srv = PSA_F_WAIT_ANY_SID_MSK,
+        .flags_rot_srv = CRYPTO_WAIT_ANY_SID_MSK,
         .flags_interrupts = 0,
         .rot_services = NULL,
-        .rot_services_count = PSA_F_ROT_SRV_COUNT,
-        .extern_sids = psa_f_external_sids,
-        .extern_sids_count = PSA_F_EXT_ROT_SRV_COUNT,
+        .rot_services_count = CRYPTO_ROT_SRV_COUNT,
+        .extern_sids = crypto_external_sids,
+        .extern_sids_count = CRYPTO_EXT_ROT_SRV_COUNT,
         .irq_mapper = NULL,
     },
     {
@@ -78,7 +78,7 @@ const uint32_t mem_region_count = 0;
 
 // forward declaration of partition initializers
 void test_its_reset_init(spm_partition_t *partition);
-void psa_f_init(spm_partition_t *partition);
+void crypto_init(spm_partition_t *partition);
 void its_init(spm_partition_t *partition);
 
 uint32_t init_partitions(spm_partition_t **partitions)
@@ -88,7 +88,7 @@ uint32_t init_partitions(spm_partition_t **partitions)
     }
 
     test_its_reset_init(&(g_partitions[0]));
-    psa_f_init(&(g_partitions[1]));
+    crypto_init(&(g_partitions[1]));
     its_init(&(g_partitions[2]));
 
     *partitions = g_partitions;

TESTS/psa/spm_client/COMPONENT_SPE/psa_setup.c
@@ -28,10 +28,10 @@
 #include "handles_manager.h"
 #include "cmsis.h"
 #include "psa_client_tests_part1_partition.h"
-#include "psa_psa_f_partition.h"
+#include "psa_crypto_partition.h"
 #include "psa_its_partition.h"
 
-extern const uint32_t psa_f_external_sids[4];
+extern const uint32_t crypto_external_sids[4];
 
 spm_partition_t g_partitions[3] = {
     {
@@ -46,14 +46,14 @@ spm_partition_t g_partitions[3] = {
         .irq_mapper = NULL,
     },
     {
-        .partition_id = PSA_F_ID,
+        .partition_id = CRYPTO_ID,
         .thread_id = 0,
-        .flags_rot_srv = PSA_F_WAIT_ANY_SID_MSK,
+        .flags_rot_srv = CRYPTO_WAIT_ANY_SID_MSK,
         .flags_interrupts = 0,
         .rot_services = NULL,
-        .rot_services_count = PSA_F_ROT_SRV_COUNT,
-        .extern_sids = psa_f_external_sids,
-        .extern_sids_count = PSA_F_EXT_ROT_SRV_COUNT,
+        .rot_services_count = CRYPTO_ROT_SRV_COUNT,
+        .extern_sids = crypto_external_sids,
+        .extern_sids_count = CRYPTO_EXT_ROT_SRV_COUNT,
         .irq_mapper = NULL,
     },
     {
@@ -78,7 +78,7 @@ const uint32_t mem_region_count = 0;
 
 // forward declaration of partition initializers
 void client_tests_part1_init(spm_partition_t *partition);
-void psa_f_init(spm_partition_t *partition);
+void crypto_init(spm_partition_t *partition);
 void its_init(spm_partition_t *partition);
 
 uint32_t init_partitions(spm_partition_t **partitions)
@@ -88,7 +88,7 @@ uint32_t init_partitions(spm_partition_t **partitions)
     }
 
     client_tests_part1_init(&(g_partitions[0]));
-    psa_f_init(&(g_partitions[1]));
+    crypto_init(&(g_partitions[1]));
     its_init(&(g_partitions[2]));
 
     *partitions = g_partitions;

TESTS/psa/spm_server/COMPONENT_SPE/psa_setup.c
@@ -29,11 +29,11 @@
 #include "cmsis.h"
 #include "psa_server_test_part1_partition.h"
 #include "psa_server_test_part2_partition.h"
-#include "psa_psa_f_partition.h"
+#include "psa_crypto_partition.h"
 #include "psa_its_partition.h"
 
 extern const uint32_t server_test_part1_external_sids[2];
-extern const uint32_t psa_f_external_sids[4];
+extern const uint32_t crypto_external_sids[4];
 
 spm_partition_t g_partitions[4] = {
     {
@@ -59,14 +59,14 @@ spm_partition_t g_partitions[4] = {
         .irq_mapper = NULL,
     },
     {
-        .partition_id = PSA_F_ID,
+        .partition_id = CRYPTO_ID,
         .thread_id = 0,
-        .flags_rot_srv = PSA_F_WAIT_ANY_SID_MSK,
+        .flags_rot_srv = CRYPTO_WAIT_ANY_SID_MSK,
         .flags_interrupts = 0,
         .rot_services = NULL,
-        .rot_services_count = PSA_F_ROT_SRV_COUNT,
-        .extern_sids = psa_f_external_sids,
-        .extern_sids_count = PSA_F_EXT_ROT_SRV_COUNT,
+        .rot_services_count = CRYPTO_ROT_SRV_COUNT,
+        .extern_sids = crypto_external_sids,
+        .extern_sids_count = CRYPTO_EXT_ROT_SRV_COUNT,
         .irq_mapper = NULL,
     },
     {
@@ -92,7 +92,7 @@ const uint32_t mem_region_count = 0;
 // forward declaration of partition initializers
 void server_test_part1_init(spm_partition_t *partition);
 void server_test_part2_init(spm_partition_t *partition);
-void psa_f_init(spm_partition_t *partition);
+void crypto_init(spm_partition_t *partition);
 void its_init(spm_partition_t *partition);
 
 uint32_t init_partitions(spm_partition_t **partitions)
@@ -103,7 +103,7 @@ uint32_t init_partitions(spm_partition_t **partitions)
 
     server_test_part1_init(&(g_partitions[0]));
     server_test_part2_init(&(g_partitions[1]));
-    psa_f_init(&(g_partitions[2]));
+    crypto_init(&(g_partitions[2]));
     its_init(&(g_partitions[3]));
 
     *partitions = g_partitions;

TESTS/psa/spm_smoke/COMPONENT_SPE/psa_setup.c
@@ -28,10 +28,10 @@
 #include "handles_manager.h"
 #include "cmsis.h"
 #include "psa_smoke_test_part1_partition.h"
-#include "psa_psa_f_partition.h"
+#include "psa_crypto_partition.h"
 #include "psa_its_partition.h"
 
-extern const uint32_t psa_f_external_sids[4];
+extern const uint32_t crypto_external_sids[4];
 
 spm_partition_t g_partitions[3] = {
     {
@@ -46,14 +46,14 @@ spm_partition_t g_partitions[3] = {
         .irq_mapper = NULL,
     },
     {
-        .partition_id = PSA_F_ID,
+        .partition_id = CRYPTO_ID,
         .thread_id = 0,
-        .flags_rot_srv = PSA_F_WAIT_ANY_SID_MSK,
+        .flags_rot_srv = CRYPTO_WAIT_ANY_SID_MSK,
         .flags_interrupts = 0,
         .rot_services = NULL,
-        .rot_services_count = PSA_F_ROT_SRV_COUNT,
-        .extern_sids = psa_f_external_sids,
-        .extern_sids_count = PSA_F_EXT_ROT_SRV_COUNT,
+        .rot_services_count = CRYPTO_ROT_SRV_COUNT,
+        .extern_sids = crypto_external_sids,
+        .extern_sids_count = CRYPTO_EXT_ROT_SRV_COUNT,
         .irq_mapper = NULL,
     },
     {
@@ -78,7 +78,7 @@ const uint32_t mem_region_count = 0;
 
 // forward declaration of partition initializers
 void smoke_test_part1_init(spm_partition_t *partition);
-void psa_f_init(spm_partition_t *partition);
+void crypto_init(spm_partition_t *partition);
 void its_init(spm_partition_t *partition);
 
 uint32_t init_partitions(spm_partition_t **partitions)
@@ -88,7 +88,7 @@ uint32_t init_partitions(spm_partition_t **partitions)
     }
 
     smoke_test_part1_init(&(g_partitions[0]));
-    psa_f_init(&(g_partitions[1]));
+    crypto_init(&(g_partitions[1]));
     its_init(&(g_partitions[2]));
 
     *partitions = g_partitions;

PDeBonte/mbed-os-STM32L4-only

TESTS/psa/crypto_init/COMPONENT_SPE/psa_setup.c
@@ -27,21 +27,21 @@
 #include "spm_internal.h"
 #include "handles_manager.h"
 #include "cmsis.h"
-#include "psa_psa_f_partition.h"
+#include "psa_crypto_srv_partition.h"
 #include "psa_its_partition.h"
 
-extern const uint32_t psa_f_external_sids[4];
+extern const uint32_t crypto_srv_external_sids[4];
 
 spm_partition_t g_partitions[2] = {
     {
-        .partition_id = PSA_F_ID,
+        .partition_id = CRYPTO_SRV_ID,
         .thread_id = 0,
-        .flags_rot_srv = PSA_F_WAIT_ANY_SID_MSK,
+        .flags_rot_srv = CRYPTO_SRV_WAIT_ANY_SID_MSK,
         .flags_interrupts = 0,
         .rot_services = NULL,
-        .rot_services_count = PSA_F_ROT_SRV_COUNT,
-        .extern_sids = psa_f_external_sids,
-        .extern_sids_count = PSA_F_EXT_ROT_SRV_COUNT,
+        .rot_services_count = CRYPTO_SRV_ROT_SRV_COUNT,
+        .extern_sids = crypto_srv_external_sids,
+        .extern_sids_count = CRYPTO_SRV_EXT_ROT_SRV_COUNT,
         .irq_mapper = NULL,
     },
     {
@@ -65,7 +65,7 @@ const mem_region_t *mem_regions = NULL;
 const uint32_t mem_region_count = 0;
 
 // forward declaration of partition initializers
-void psa_f_init(spm_partition_t *partition);
+void crypto_srv_init(spm_partition_t *partition);
 void its_init(spm_partition_t *partition);
 
 uint32_t init_partitions(spm_partition_t **partitions)
@@ -74,7 +74,7 @@ uint32_t init_partitions(spm_partition_t **partitions)
         SPM_PANIC("partitions is NULL!\n");
     }
 
-    psa_f_init(&(g_partitions[0]));
+    crypto_srv_init(&(g_partitions[0]));
     its_init(&(g_partitions[1]));
 
     *partitions = g_partitions;

TESTS/psa/entropy_inject/COMPONENT_SPE/psa_setup.c
@@ -28,10 +28,10 @@
 #include "handles_manager.h"
 #include "cmsis.h"
 #include "psa_test_its_reset_partition.h"
-#include "psa_psa_f_partition.h"
+#include "psa_crypto_srv_partition.h"
 #include "psa_its_partition.h"
 
-extern const uint32_t psa_f_external_sids[4];
+extern const uint32_t crypto_srv_external_sids[4];
 
 spm_partition_t g_partitions[3] = {
     {
@@ -46,14 +46,14 @@ spm_partition_t g_partitions[3] = {
         .irq_mapper = NULL,
     },
     {
-        .partition_id = PSA_F_ID,
+        .partition_id = CRYPTO_SRV_ID,
         .thread_id = 0,
-        .flags_rot_srv = PSA_F_WAIT_ANY_SID_MSK,
+        .flags_rot_srv = CRYPTO_SRV_WAIT_ANY_SID_MSK,
         .flags_interrupts = 0,
         .rot_services = NULL,
-        .rot_services_count = PSA_F_ROT_SRV_COUNT,
-        .extern_sids = psa_f_external_sids,
-        .extern_sids_count = PSA_F_EXT_ROT_SRV_COUNT,
+        .rot_services_count = CRYPTO_SRV_ROT_SRV_COUNT,
+        .extern_sids = crypto_srv_external_sids,
+        .extern_sids_count = CRYPTO_SRV_EXT_ROT_SRV_COUNT,
         .irq_mapper = NULL,
     },
     {
@@ -78,7 +78,7 @@ const uint32_t mem_region_count = 0;
 
 // forward declaration of partition initializers
 void test_its_reset_init(spm_partition_t *partition);
-void psa_f_init(spm_partition_t *partition);
+void crypto_srv_init(spm_partition_t *partition);
 void its_init(spm_partition_t *partition);
 
 uint32_t init_partitions(spm_partition_t **partitions)
@@ -88,7 +88,7 @@ uint32_t init_partitions(spm_partition_t **partitions)
     }
 
     test_its_reset_init(&(g_partitions[0]));
-    psa_f_init(&(g_partitions[1]));
+    crypto_srv_init(&(g_partitions[1]));
     its_init(&(g_partitions[2]));
 
     *partitions = g_partitions;

TESTS/psa/spm_client/COMPONENT_SPE/psa_setup.c
@@ -28,10 +28,10 @@
 #include "handles_manager.h"
 #include "cmsis.h"
 #include "psa_client_tests_part1_partition.h"
-#include "psa_psa_f_partition.h"
+#include "psa_crypto_srv_partition.h"
 #include "psa_its_partition.h"
 
-extern const uint32_t psa_f_external_sids[4];
+extern const uint32_t crypto_srv_external_sids[4];
 
 spm_partition_t g_partitions[3] = {
     {
@@ -46,14 +46,14 @@ spm_partition_t g_partitions[3] = {
         .irq_mapper = NULL,
     },
     {
-        .partition_id = PSA_F_ID,
+        .partition_id = CRYPTO_SRV_ID,
         .thread_id = 0,
-        .flags_rot_srv = PSA_F_WAIT_ANY_SID_MSK,
+        .flags_rot_srv = CRYPTO_SRV_WAIT_ANY_SID_MSK,
         .flags_interrupts = 0,
         .rot_services = NULL,
-        .rot_services_count = PSA_F_ROT_SRV_COUNT,
-        .extern_sids = psa_f_external_sids,
-        .extern_sids_count = PSA_F_EXT_ROT_SRV_COUNT,
+        .rot_services_count = CRYPTO_SRV_ROT_SRV_COUNT,
+        .extern_sids = crypto_srv_external_sids,
+        .extern_sids_count = CRYPTO_SRV_EXT_ROT_SRV_COUNT,
         .irq_mapper = NULL,
     },
     {
@@ -78,7 +78,7 @@ const uint32_t mem_region_count = 0;
 
 // forward declaration of partition initializers
 void client_tests_part1_init(spm_partition_t *partition);
-void psa_f_init(spm_partition_t *partition);
+void crypto_srv_init(spm_partition_t *partition);
 void its_init(spm_partition_t *partition);
 
 uint32_t init_partitions(spm_partition_t **partitions)
@@ -88,7 +88,7 @@ uint32_t init_partitions(spm_partition_t **partitions)
     }
 
     client_tests_part1_init(&(g_partitions[0]));
-    psa_f_init(&(g_partitions[1]));
+    crypto_srv_init(&(g_partitions[1]));
     its_init(&(g_partitions[2]));
 
     *partitions = g_partitions;

TESTS/psa/spm_server/COMPONENT_SPE/psa_setup.c
@@ -29,11 +29,11 @@
 #include "cmsis.h"
 #include "psa_server_test_part1_partition.h"
 #include "psa_server_test_part2_partition.h"
-#include "psa_psa_f_partition.h"
+#include "psa_crypto_srv_partition.h"
 #include "psa_its_partition.h"
 
 extern const uint32_t server_test_part1_external_sids[2];
-extern const uint32_t psa_f_external_sids[4];
+extern const uint32_t crypto_srv_external_sids[4];
 
 spm_partition_t g_partitions[4] = {
     {
@@ -59,14 +59,14 @@ spm_partition_t g_partitions[4] = {
         .irq_mapper = NULL,
     },
     {
-        .partition_id = PSA_F_ID,
+        .partition_id = CRYPTO_SRV_ID,
         .thread_id = 0,
-        .flags_rot_srv = PSA_F_WAIT_ANY_SID_MSK,
+        .flags_rot_srv = CRYPTO_SRV_WAIT_ANY_SID_MSK,
         .flags_interrupts = 0,
         .rot_services = NULL,
-        .rot_services_count = PSA_F_ROT_SRV_COUNT,
-        .extern_sids = psa_f_external_sids,
-        .extern_sids_count = PSA_F_EXT_ROT_SRV_COUNT,
+        .rot_services_count = CRYPTO_SRV_ROT_SRV_COUNT,
+        .extern_sids = crypto_srv_external_sids,
+        .extern_sids_count = CRYPTO_SRV_EXT_ROT_SRV_COUNT,
         .irq_mapper = NULL,
     },
     {
@@ -92,7 +92,7 @@ const uint32_t mem_region_count = 0;
 // forward declaration of partition initializers
 void server_test_part1_init(spm_partition_t *partition);
 void server_test_part2_init(spm_partition_t *partition);
-void psa_f_init(spm_partition_t *partition);
+void crypto_srv_init(spm_partition_t *partition);
 void its_init(spm_partition_t *partition);
 
 uint32_t init_partitions(spm_partition_t **partitions)
@@ -103,7 +103,7 @@ uint32_t init_partitions(spm_partition_t **partitions)
 
     server_test_part1_init(&(g_partitions[0]));
     server_test_part2_init(&(g_partitions[1]));
-    psa_f_init(&(g_partitions[2]));
+    crypto_srv_init(&(g_partitions[2]));
     its_init(&(g_partitions[3]));
 
     *partitions = g_partitions;

TESTS/psa/spm_smoke/COMPONENT_SPE/psa_setup.c
@@ -28,10 +28,10 @@
 #include "handles_manager.h"
 #include "cmsis.h"
 #include "psa_smoke_test_part1_partition.h"
-#include "psa_psa_f_partition.h"
+#include "psa_crypto_srv_partition.h"
 #include "psa_its_partition.h"
 
-extern const uint32_t psa_f_external_sids[4];
+extern const uint32_t crypto_srv_external_sids[4];
 
 spm_partition_t g_partitions[3] = {
     {
@@ -46,14 +46,14 @@ spm_partition_t g_partitions[3] = {
         .irq_mapper = NULL,
     },
     {
-        .partition_id = PSA_F_ID,
+        .partition_id = CRYPTO_SRV_ID,
         .thread_id = 0,
-        .flags_rot_srv = PSA_F_WAIT_ANY_SID_MSK,
+        .flags_rot_srv = CRYPTO_SRV_WAIT_ANY_SID_MSK,
         .flags_interrupts = 0,
         .rot_services = NULL,
-        .rot_services_count = PSA_F_ROT_SRV_COUNT,
-        .extern_sids = psa_f_external_sids,
-        .extern_sids_count = PSA_F_EXT_ROT_SRV_COUNT,
+        .rot_services_count = CRYPTO_SRV_ROT_SRV_COUNT,
+        .extern_sids = crypto_srv_external_sids,
+        .extern_sids_count = CRYPTO_SRV_EXT_ROT_SRV_COUNT,
         .irq_mapper = NULL,
     },
     {
@@ -78,7 +78,7 @@ const uint32_t mem_region_count = 0;
 
 // forward declaration of partition initializers
 void smoke_test_part1_init(spm_partition_t *partition);
-void psa_f_init(spm_partition_t *partition);
+void crypto_srv_init(spm_partition_t *partition);
 void its_init(spm_partition_t *partition);
 
 uint32_t init_partitions(spm_partition_t **partitions)
@@ -88,7 +88,7 @@ uint32_t init_partitions(spm_partition_t **partitions)
     }
 
     smoke_test_part1_init(&(g_partitions[0]));
-    psa_f_init(&(g_partitions[1]));
+    crypto_srv_init(&(g_partitions[1]));
     its_init(&(g_partitions[2]));
 
     *partitions = g_partitions;
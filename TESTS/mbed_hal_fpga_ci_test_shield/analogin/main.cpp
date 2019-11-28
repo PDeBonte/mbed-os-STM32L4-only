mprse/mbed-os

Adapt FPGA tests for spi, i2c, analogin, pwm, uart to test direct init (explicit pinmap).

TESTS/mbed_hal_fpga_ci_test_shield/analogin/main.cpp
@@ -29,6 +29,7 @@
 
 #include "mbed.h"
 #include "pinmap.h"
+#include "hal/explicit_pinmap.h"
 #include "test_utils.h"
 #include "MbedTester.h"
 
@@ -36,8 +37,8 @@ using namespace utest::v1;
 
 #define analogin_debug_printf(...)
 
-#define DELTA_FLOAT                     0.05f    // 5%
-#define DELTA_U16                       3277     // 5%
+#define DELTA_FLOAT                     (0.1f)       // 10%
+#define DELTA_U16                       (2*3277)     // 10%
 
 const PinList *form_factor = pinmap_ff_default_pins();
 const PinList *restricted = pinmap_restricted_pins();
@@ -51,6 +52,7 @@ void analogin_init(PinName pin)
     analogin_init(&analogin, pin);
 }
 
+template<bool init_direct>
 void analogin_test(PinName pin)
 {
     tester.reset();
@@ -60,7 +62,13 @@ void analogin_test(PinName pin)
     /* Test analog input */
 
     analogin_t analogin;
-    analogin_init(&analogin, pin);
+
+    if (init_direct) {
+        const PinMap pinmap = get_analogin_pinmap(pin);
+        analogin_init_direct(&analogin, &pinmap);
+    } else {
+        analogin_init(&analogin, pin);
+    }
 
     tester.gpio_write(MbedTester::LogicalPinGPIO0, 1, true);
     TEST_ASSERT_FLOAT_WITHIN(DELTA_FLOAT, 1.0f, analogin_read(&analogin));
@@ -78,7 +86,8 @@ Case cases[] = {
     // This will be run for all pins
     Case("AnalogIn - init test", all_ports<AnaloginPort, DefaultFormFactor, analogin_init>),
     // This will be run for single pin
-    Case("AnalogIn - read test", all_ports<AnaloginPort, DefaultFormFactor, analogin_test>),
+    Case("AnalogIn - read test", all_ports<AnaloginPort, DefaultFormFactor, analogin_test<false>>),
+    Case("AnalogIn (direct init) - read test", all_ports<AnaloginPort, DefaultFormFactor, analogin_test<true>>),
 };
 
 utest::v1::status_t greentea_test_setup(const size_t number_of_cases)

TESTS/mbed_hal_fpga_ci_test_shield/i2c/main.cpp
@@ -31,9 +31,9 @@
 #include "mbed.h"
 #include "i2c_api.h"
 #include "pinmap.h"
+#include "hal/explicit_pinmap.h"
 #include "test_utils.h"
 #include "I2CTester.h"
-
 
 using namespace utest::v1;
 
@@ -53,6 +53,7 @@ void test_i2c_init_free(PinName sda, PinName scl)
     i2c_frequency(&obj, 100000);
 }
 
+template<bool init_direct>
 void i2c_test_write(PinName sda, PinName scl)
 {
     // Remap pins for test
@@ -66,7 +67,17 @@ void i2c_test_write(PinName sda, PinName scl)
     // Initialize mbed I2C pins
     i2c_t i2c;
     memset(&i2c, 0, sizeof(i2c));
-    i2c_init(&i2c, sda, scl);
+    if (init_direct) {
+#if EXPLICIT_PINMAP_READY
+        const i2c_pinmap_t pinmap = get_i2c_pinmap(sda, scl);
+        i2c_init_direct(&i2c, &pinmap);
+#else
+        //skip this test case if explicit pinmap is not supported
+        return;
+#endif
+    } else {
+        i2c_init(&i2c, sda, scl);
+    }
     i2c_frequency(&i2c, 100000);
 
     // Reset tester stats and select I2C
@@ -140,6 +151,7 @@ void i2c_test_write(PinName sda, PinName scl)
     tester.pin_set_pull(scl, MbedTester::PullNone);
 }
 
+template<bool init_direct>
 void i2c_test_read(PinName sda, PinName scl)
 {
     // Remap pins for test
@@ -153,7 +165,12 @@ void i2c_test_read(PinName sda, PinName scl)
     // Initialize mbed I2C pins
     i2c_t i2c;
     memset(&i2c, 0, sizeof(i2c));
-    i2c_init(&i2c, sda, scl);
+    if (init_direct) {
+        const i2c_pinmap_t pinmap = get_i2c_pinmap(sda, scl);
+        i2c_init_direct(&i2c, &pinmap);
+    } else {
+        i2c_init(&i2c, sda, scl);
+    }
     i2c_frequency(&i2c, 100000);
 
     // Reset tester stats and select I2C
@@ -425,8 +442,10 @@ void i2c_test_byte_read(PinName sda, PinName scl)
 
 Case cases[] = {
     Case("i2c - init/free test all pins", all_ports<I2CPort, DefaultFormFactor, test_i2c_init_free>),
-    Case("i2c - test write i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_write>),
-    Case("i2c - test read i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_read>),
+    Case("i2c - test write i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_write<false>>),
+    Case("i2c (direct init) - test write i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_write<true>>),
+    Case("i2c - test read i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_read<false>>),
+    Case("i2c (direct init) - test read i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_read<true>>),
     Case("i2c - test single byte write i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_byte_write>),
     Case("i2c - test single byte read i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_byte_read>)
 };

TESTS/mbed_hal_fpga_ci_test_shield/spi/main.cpp
@@ -32,6 +32,7 @@ using namespace utest::v1;
 
 #include "SPIMasterTester.h"
 #include "pinmap.h"
+#include "hal/explicit_pinmap.h"
 #include "test_utils.h"
 
 typedef enum {
@@ -69,7 +70,7 @@ void spi_test_init_free(PinName mosi, PinName miso, PinName sclk, PinName ssel)
     spi_free(&spi);
 }
 
-void spi_test_common(PinName mosi, PinName miso, PinName sclk, PinName ssel, SPITester::SpiMode spi_mode, uint32_t sym_size, transfer_type_t transfer_type, uint32_t frequency)
+void spi_test_common(PinName mosi, PinName miso, PinName sclk, PinName ssel, SPITester::SpiMode spi_mode, uint32_t sym_size, transfer_type_t transfer_type, uint32_t frequency, bool init_direct)
 {
     uint32_t sym_mask = ((1 << sym_size) - 1);
 
@@ -82,7 +83,13 @@ void spi_test_common(PinName mosi, PinName miso, PinName sclk, PinName ssel, SPI
 
     // Initialize mbed SPI pins
 
-    spi_init(&spi, mosi, miso, sclk, ssel);
+    if (init_direct) {
+        const spi_pinmap_t pinmap = get_spi_pinmap(mosi, miso, sclk, ssel);
+        spi_init_direct(&spi, &pinmap);
+    } else {
+        spi_init(&spi, mosi, miso, sclk, ssel);
+    }
+
     spi_format(&spi, sym_size, spi_mode, 0);
     spi_frequency(&spi, frequency);
 
@@ -160,10 +167,10 @@ void spi_test_common(PinName mosi, PinName miso, PinName sclk, PinName ssel, SPI
     tester.reset();
 }
 
-template<SPITester::SpiMode spi_mode, uint32_t sym_size, transfer_type_t transfer_type, uint32_t frequency>
+template<SPITester::SpiMode spi_mode, uint32_t sym_size, transfer_type_t transfer_type, uint32_t frequency, bool init_direct>
 void spi_test_common(PinName mosi, PinName miso, PinName sclk, PinName ssel)
 {
-    spi_test_common(mosi, miso, sclk, ssel, spi_mode, sym_size, transfer_type, frequency);
+    spi_test_common(mosi, miso, sclk, ssel, spi_mode, sym_size, transfer_type, frequency, init_direct);
 }
 
 Case cases[] = {
@@ -171,22 +178,23 @@ Case cases[] = {
     Case("SPI - init/free test all pins", all_ports<SPIPort, DefaultFormFactor, spi_test_init_free>),
 
     // This will be run for all peripherals
-    Case("SPI - basic test", all_peripherals<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ> >),
+    Case("SPI - basic test", all_peripherals<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, false> >),
+    Case("SPI - basic test (direct init)", all_peripherals<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, true> >),
 
     // This will be run for single pin configuration
-    Case("SPI - mode testing (MODE_1)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode1, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ> >),
-    Case("SPI - mode testing (MODE_2)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode2, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ> >),
-    Case("SPI - mode testing (MODE_3)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode3, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ> >),
+    Case("SPI - mode testing (MODE_1)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode1, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, false> >),
+    Case("SPI - mode testing (MODE_2)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode2, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, false> >),
+    Case("SPI - mode testing (MODE_3)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode3, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, false> >),
 
-    Case("SPI - symbol size testing (16)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 16, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ> >),
+    Case("SPI - symbol size testing (16)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 16, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, false> >),
 
-    Case("SPI - frequency testing (500 kHz)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_500_KHZ> >),
-    Case("SPI - frequency testing (2 MHz)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_2_MHZ> >),
+    Case("SPI - frequency testing (500 kHz)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_500_KHZ, false> >),
+    Case("SPI - frequency testing (2 MHz)", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_2_MHZ, false> >),
 
-    Case("SPI - block write", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_BLOCK_WRITE_SYNC, FREQ_1_MHZ> >),
+    Case("SPI - block write", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_BLOCK_WRITE_SYNC, FREQ_1_MHZ, false> >),
 
 #if DEVICE_SPI_ASYNCH
-    Case("SPI - async mode", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_TRANSFER_ASYNC, FREQ_1_MHZ> >)
+    Case("SPI - async mode", one_peripheral<SPIPort, DefaultFormFactor, spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_TRANSFER_ASYNC, FREQ_1_MHZ, false> >)
 #endif
 };
 

PDeBonte/mbed-os-STM32L4-only

TESTS/mbed_hal_fpga_ci_test_shield/analogin/main.cpp
@@ -28,6 +28,7 @@
 #include "greentea-client/test_env.h"
 #include "mbed.h"
 #include "pinmap.h"
+#include "hal/explicit_pinmap.h"
 #include "test_utils.h"
 #include "MbedTester.h"
 #include "analogin_fpga_test.h"
@@ -36,8 +37,8 @@ using namespace utest::v1;
 
 #define analogin_debug_printf(...)
 
-#define DELTA_FLOAT                     0.05f    // 5%
-#define DELTA_U16                       3277     // 5%
+#define DELTA_FLOAT                     (0.1f)       // 10%
+#define DELTA_U16                       (2*3277)     // 10%
 
 const PinList *form_factor = pinmap_ff_default_pins();
 const PinList *restricted = pinmap_restricted_pins();
@@ -52,6 +53,7 @@ void fpga_analogin_init_test(PinName pin)
     analogin_free(&analogin);
 }
 
+template<bool init_direct>
 void fpga_analogin_test(PinName pin)
 {
     tester.reset();
@@ -61,7 +63,13 @@ void fpga_analogin_test(PinName pin)
     /* Test analog input */
 
     analogin_t analogin;
-    analogin_init(&analogin, pin);
+
+    if (init_direct) {
+        const PinMap pinmap = get_analogin_pinmap(pin);
+        analogin_init_direct(&analogin, &pinmap);
+    } else {
+        analogin_init(&analogin, pin);
+    }
 
     tester.gpio_write(MbedTester::LogicalPinGPIO0, 1, true);
     TEST_ASSERT_FLOAT_WITHIN(DELTA_FLOAT, 1.0f, analogin_read(&analogin));
@@ -81,7 +89,8 @@ Case cases[] = {
     // This will be run for all pins
     Case("AnalogIn - init test", all_ports<AnaloginPort, DefaultFormFactor, fpga_analogin_init_test>),
     // This will be run for single pin
-    Case("AnalogIn - read test", all_ports<AnaloginPort, DefaultFormFactor, fpga_analogin_test>),
+    Case("AnalogIn - read test", all_ports<AnaloginPort, DefaultFormFactor, fpga_analogin_test<false>>),
+    Case("AnalogIn (direct init) - read test", all_ports<AnaloginPort, DefaultFormFactor, fpga_analogin_test<true>>),
 };
 
 utest::v1::status_t greentea_test_setup(const size_t number_of_cases)

TESTS/mbed_hal_fpga_ci_test_shield/i2c/main.cpp
@@ -29,6 +29,7 @@
 #include "mbed.h"
 #include "i2c_api.h"
 #include "pinmap.h"
+#include "hal/explicit_pinmap.h"
 #include "test_utils.h"
 #include "I2CTester.h"
 #include "i2c_fpga_test.h"
@@ -53,6 +54,7 @@ void fpga_test_i2c_init_free(PinName sda, PinName scl)
     i2c_free(&obj);
 }
 
+template<bool init_direct>
 void fpga_i2c_test_write(PinName sda, PinName scl)
 {
     // Remap pins for test
@@ -66,7 +68,17 @@ void fpga_i2c_test_write(PinName sda, PinName scl)
     // Initialize mbed I2C pins
     i2c_t i2c;
     memset(&i2c, 0, sizeof(i2c));
-    i2c_init(&i2c, sda, scl);
+    if (init_direct) {
+#if EXPLICIT_PINMAP_READY
+        const i2c_pinmap_t pinmap = get_i2c_pinmap(sda, scl);
+        i2c_init_direct(&i2c, &pinmap);
+#else
+        //skip this test case if explicit pinmap is not supported
+        return;
+#endif
+    } else {
+        i2c_init(&i2c, sda, scl);
+    }
     i2c_frequency(&i2c, 100000);
 
     // Reset tester stats and select I2C
@@ -141,6 +153,7 @@ void fpga_i2c_test_write(PinName sda, PinName scl)
     i2c_free(&i2c);
 }
 
+template<bool init_direct>
 void fpga_i2c_test_read(PinName sda, PinName scl)
 {
     // Remap pins for test
@@ -154,7 +167,12 @@ void fpga_i2c_test_read(PinName sda, PinName scl)
     // Initialize mbed I2C pins
     i2c_t i2c;
     memset(&i2c, 0, sizeof(i2c));
-    i2c_init(&i2c, sda, scl);
+    if (init_direct) {
+        const i2c_pinmap_t pinmap = get_i2c_pinmap(sda, scl);
+        i2c_init_direct(&i2c, &pinmap);
+    } else {
+        i2c_init(&i2c, sda, scl);
+    }
     i2c_frequency(&i2c, 100000);
 
     // Reset tester stats and select I2C
@@ -428,11 +446,13 @@ void fpga_i2c_test_byte_read(PinName sda, PinName scl)
 }
 
 Case cases[] = {
-    Case("i2c - init/free test all pins", one_peripheral<I2CPort, DefaultFormFactor, fpga_test_i2c_init_free>),
-    Case("i2c - test write i2c API", one_peripheral<I2CPort, DefaultFormFactor, fpga_i2c_test_write>),
-    Case("i2c - test read i2c API", one_peripheral<I2CPort, DefaultFormFactor, fpga_i2c_test_read>),
-    Case("i2c - test single byte write i2c API", one_peripheral<I2CPort, DefaultFormFactor, fpga_i2c_test_byte_write>),
-    Case("i2c - test single byte read i2c API", one_peripheral<I2CPort, DefaultFormFactor, fpga_i2c_test_byte_read>)
+    Case("i2c - init/free test all pins", all_ports<I2CPort, DefaultFormFactor, fpga_test_i2c_init_free>),
+    Case("i2c - test write i2c API", all_peripherals<I2CPort, DefaultFormFactor, fpga_i2c_test_write<false>>),
+    Case("i2c (direct init) - test write i2c API", all_peripherals<I2CPort, DefaultFormFactor, fpga_i2c_test_write<true>>),
+    Case("i2c - test read i2c API", all_peripherals<I2CPort, DefaultFormFactor, fpga_i2c_test_read<false>>),
+    Case("i2c (direct init) - test read i2c API", all_peripherals<I2CPort, DefaultFormFactor, fpga_i2c_test_read<true>>),
+    Case("i2c - test single byte write i2c API", all_peripherals<I2CPort, DefaultFormFactor, fpga_i2c_test_byte_write>),
+    Case("i2c - test single byte read i2c API", all_peripherals<I2CPort, DefaultFormFactor, fpga_i2c_test_byte_read>)
 };
 
 utest::v1::status_t greentea_test_setup(const size_t number_of_cases)

TESTS/mbed_hal_fpga_ci_test_shield/spi/main.cpp
@@ -29,6 +29,7 @@
 #include "mbed.h"
 #include "SPIMasterTester.h"
 #include "pinmap.h"
+#include "hal/explicit_pinmap.h"
 #include "test_utils.h"
 #include "spi_fpga_test.h"
 
@@ -104,7 +105,7 @@ void fpga_spi_test_init_free(PinName mosi, PinName miso, PinName sclk, PinName s
     spi_free(&spi);
 }
 
-void fpga_spi_test_common(PinName mosi, PinName miso, PinName sclk, PinName ssel, SPITester::SpiMode spi_mode, uint32_t sym_size, transfer_type_t transfer_type, uint32_t frequency)
+void fpga_spi_test_common(PinName mosi, PinName miso, PinName sclk, PinName ssel, SPITester::SpiMode spi_mode, uint32_t sym_size, transfer_type_t transfer_type, uint32_t frequency, bool init_direct)
 {
     spi_capabilities_t capabilities;
 
@@ -126,7 +127,13 @@ void fpga_spi_test_common(PinName mosi, PinName miso, PinName sclk, PinName ssel
 
     // Initialize mbed SPI pins
 
-    spi_init(&spi, mosi, miso, sclk, ssel);
+    if (init_direct) {
+        const spi_pinmap_t pinmap = get_spi_pinmap(mosi, miso, sclk, ssel);
+        spi_init_direct(&spi, &pinmap);
+    } else {
+        spi_init(&spi, mosi, miso, sclk, ssel);
+    }
+
     spi_format(&spi, sym_size, spi_mode, 0);
     spi_frequency(&spi, frequency);
 
@@ -204,10 +211,10 @@ void fpga_spi_test_common(PinName mosi, PinName miso, PinName sclk, PinName ssel
     tester.reset();
 }
 
-template<SPITester::SpiMode spi_mode, uint32_t sym_size, transfer_type_t transfer_type, uint32_t frequency>
+template<SPITester::SpiMode spi_mode, uint32_t sym_size, transfer_type_t transfer_type, uint32_t frequency, bool init_direct>
 void fpga_spi_test_common(PinName mosi, PinName miso, PinName sclk, PinName ssel)
 {
-    fpga_spi_test_common(mosi, miso, sclk, ssel, spi_mode, sym_size, transfer_type, frequency);
+    fpga_spi_test_common(mosi, miso, sclk, ssel, spi_mode, sym_size, transfer_type, frequency, init_direct);
 }
 
 Case cases[] = {
@@ -215,18 +222,23 @@ Case cases[] = {
     Case("SPI - init/free test all pins", all_ports<SPIPort, DefaultFormFactor, fpga_spi_test_init_free>),
 
     // This will be run for all peripherals
-    Case("SPI - basic test", all_peripherals<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ> >),
+    Case("SPI - basic test", all_peripherals<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, false> >),
+    Case("SPI - basic test (direct init)", all_peripherals<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, true> >),
 
     // This will be run for single pin configuration
-    Case("SPI - mode testing (MODE_1)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode1, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ> >),
-    Case("SPI - mode testing (MODE_2)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode2, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ> >),
-    Case("SPI - mode testing (MODE_3)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode3, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ> >),
-    Case("SPI - symbol size testing (16)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 16, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ> >),
-    Case("SPI - frequency testing (500 kHz)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_500_KHZ> >),
-    Case("SPI - frequency testing (2 MHz)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_2_MHZ> >),
-    Case("SPI - block write", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_BLOCK_WRITE_SYNC, FREQ_1_MHZ> >),
+    Case("SPI - mode testing (MODE_1)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode1, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, false> >),
+    Case("SPI - mode testing (MODE_2)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode2, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, false> >),
+    Case("SPI - mode testing (MODE_3)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode3, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, false> >),
+
+    Case("SPI - symbol size testing (16)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 16, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_1_MHZ, false> >),
+
+    Case("SPI - frequency testing (500 kHz)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_500_KHZ, false> >),
+    Case("SPI - frequency testing (2 MHz)", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_WRITE_SYNC, FREQ_2_MHZ, false> >),
+
+    Case("SPI - block write", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_BLOCK_WRITE_SYNC, FREQ_1_MHZ, false> >),
+
 #if DEVICE_SPI_ASYNCH
-    Case("SPI - async mode", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_TRANSFER_ASYNC, FREQ_1_MHZ> >)
+    Case("SPI - async mode", one_peripheral<SPIPort, DefaultFormFactor, fpga_spi_test_common<SPITester::Mode0, 8, TRANSFER_SPI_MASTER_TRANSFER_ASYNC, FREQ_1_MHZ, false> >)
 #endif
 };
 
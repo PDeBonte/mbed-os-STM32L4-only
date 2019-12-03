SeppoTakalo/mbed-os

Add unittests for BufferedBlockDevice

UNITTESTS/moduletests/storage/blockdevice/BufferedBlockDevice/unittest.cmake
@@ -1,0 +1,22 @@
+
+####################
+# UNIT TESTS
+####################
+
+set(unittest-includes ${unittest-includes}
+  .
+  ..
+)
+
+set(unittest-sources
+  ../features/storage/blockdevice/BufferedBlockDevice.cpp
+  ../features/storage/blockdevice/HeapBlockDevice.cpp
+  stubs/mbed_atomic_stub.c
+  stubs/mbed_assert_stub.c
+)
+
+set(unittest-test-sources
+  moduletests/storage/blockdevice/BufferedBlockDevice/moduletest.cpp
+)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DMBED_ASSERT_THROW_ERROR")

PDeBonte/mbed-os-STM32L4-only

UNITTESTS/moduletests/storage/blockdevice/BufferedBlockDevice/unittest.cmake
@@ -1,0 +1,20 @@
+
+####################
+# UNIT TESTS
+####################
+
+set(unittest-includes ${unittest-includes}
+  .
+  ..
+)
+
+set(unittest-sources
+  ../features/storage/blockdevice/BufferedBlockDevice.cpp
+  ../features/storage/blockdevice/HeapBlockDevice.cpp
+  stubs/mbed_atomic_stub.c
+  stubs/mbed_assert_stub.cpp
+)
+
+set(unittest-test-sources
+  moduletests/storage/blockdevice/BufferedBlockDevice/moduletest.cpp
+)
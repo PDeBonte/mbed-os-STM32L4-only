SeppoTakalo/mbed-os

Add module tests for TDBStore

UNITTESTS/moduletests/storage/kvstore/TDBStore/unittest.cmake
@@ -1,0 +1,27 @@
+
+####################
+# UNIT TESTS
+####################
+
+set(unittest-includes ${unittest-includes}
+  .
+  ..
+  ../features/frameworks/mbed-trace/mbed-trace
+)
+
+set(unittest-sources
+  ../features/storage/blockdevice/FlashSimBlockDevice.cpp
+  ../features/storage/blockdevice/HeapBlockDevice.cpp
+  ../features/storage/blockdevice/BufferedBlockDevice.cpp
+  ../features/storage/kvstore/tdbstore/TDBStore.cpp
+  ../features/frameworks/mbed-trace/source/mbed_trace.c
+  stubs/mbed_atomic_stub.c
+  stubs/mbed_assert_stub.c
+  stubs/mbed_error.c
+)
+
+set(unittest-test-sources
+  moduletests/storage/kvstore/TDBStore/moduletest.cpp
+)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DBYPASS_NVSTORE_CHECK")

PDeBonte/mbed-os-STM32L4-only

UNITTESTS/moduletests/storage/kvstore/TDBStore/unittest.cmake
@@ -1,0 +1,27 @@
+
+####################
+# UNIT TESTS
+####################
+
+set(unittest-includes ${unittest-includes}
+  .
+  ..
+  ../features/frameworks/mbed-trace/mbed-trace
+)
+
+set(unittest-sources
+  ../features/storage/blockdevice/FlashSimBlockDevice.cpp
+  ../features/storage/blockdevice/HeapBlockDevice.cpp
+  ../features/storage/blockdevice/BufferedBlockDevice.cpp
+  ../features/storage/kvstore/tdbstore/TDBStore.cpp
+  ../features/frameworks/mbed-trace/source/mbed_trace.c
+  stubs/mbed_atomic_stub.c
+  stubs/mbed_assert_stub.cpp
+  stubs/mbed_error.c
+)
+
+set(unittest-test-sources
+  moduletests/storage/kvstore/TDBStore/moduletest.cpp
+)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DBYPASS_NVSTORE_CHECK")